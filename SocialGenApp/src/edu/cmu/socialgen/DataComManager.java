e870bcf91459df5cd578d901355a4c86dba0c2b6
Sungho Cho <sunghcho@ebay.com>
2013-09-02 21:20:42 -0700

    removed actionbarsherlock (tab implemented with tabhost, tabwidget, and fragments)

--- SocialGenApp/src/edu/cmu/socialgen/DataComManager.java
+++ SocialGenApp/src/edu/cmu/socialgen/DataComManager.java
@@ -1,7 +1,5 @@
 package edu.cmu.socialgen;
 
-import java.net.*;
-
 public class DataComManager {
 
 
--- SocialGenApp/src/edu/cmu/socialgen/MainActivity.java
+++ SocialGenApp/src/edu/cmu/socialgen/MainActivity.java
@@ -1,31 +1,106 @@
 package edu.cmu.socialgen;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.util.Log;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.widget.TabHost;
+import edu.cmu.socialgen.tab.ChatsFragment;
+import edu.cmu.socialgen.tab.DummyTabContent;
+import edu.cmu.socialgen.tab.FolksFragment;
+import edu.cmu.socialgen.tab.HistoryFragment;
+import edu.cmu.socialgen.tab.ProfileFragment;
 
-public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    	Log.i("SenderTest", "start!");
-        try {
-        	ControlComManager CM = new ControlComManager();
-        	new Thread(CM).start();
-        } catch (Exception e) {
-        	
-        }
+public class MainActivity extends FragmentActivity {
+
+	TabHost tHost;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+ 
+        tHost = (TabHost) findViewById(android.R.id.tabhost);
+        tHost.setup();
+ 
+        /** Defining Tab Change Listener event. This is invoked when tab is changed */
+        TabHost.OnTabChangeListener tabChangeListener = new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                android.support.v4.app.FragmentManager fm =   getSupportFragmentManager();
+                FolksFragment folksFragment = (FolksFragment) fm.findFragmentByTag("folks");
+                ChatsFragment chatsFragment = (ChatsFragment) fm.findFragmentByTag("chats");
+                HistoryFragment historyFragment = (HistoryFragment) fm.findFragmentByTag("history");
+                ProfileFragment profileFragment = (ProfileFragment) fm.findFragmentByTag("profile");
+                android.support.v4.app.FragmentTransaction ft = fm.beginTransaction();
+ 
+                if(folksFragment!=null)
+                    ft.detach(folksFragment);
+ 
+                if(chatsFragment!=null)
+                    ft.detach(chatsFragment);
+ 
+                if(historyFragment!=null)
+                    ft.detach(historyFragment);
+                
+                if(profileFragment!=null)
+                    ft.detach(profileFragment);
+                
+                if(tabId.equalsIgnoreCase("folks")){
+                    if(folksFragment==null){
+                        ft.add(R.id.realtabcontent, new FolksFragment(), "folks");
+                    }else{
+                        ft.attach(folksFragment);
+                    }
+                }else if(tabId.equalsIgnoreCase("chats")){
+                    if(chatsFragment==null){
+                        ft.add(R.id.realtabcontent, new ChatsFragment(), "chats");
+                     }else{
+                        ft.attach(chatsFragment);
+                    }
+                }else if(tabId.equalsIgnoreCase("history")) {
+                	if(historyFragment==null){
+                        ft.add(R.id.realtabcontent, new HistoryFragment(), "history");
+                     }else{
+                        ft.attach(historyFragment);
+                    }
+                }else{
+                	if(profileFragment==null){
+                        ft.add(R.id.realtabcontent, new ProfileFragment(), "profile");
+                     }else{
+                        ft.attach(profileFragment);
+                    }
+                }
+                ft.commit();
+            }
+        };
+ 
+        /** Setting tabchangelistener for the tab */
+        tHost.setOnTabChangedListener(tabChangeListener);
+ 
+        /** Defining tab builder for Folks tab */
+        TabHost.TabSpec tSpecFolks = tHost.newTabSpec("folks");
+        tSpecFolks.setIndicator("Folks",getResources().getDrawable(R.drawable.folks));
+        tSpecFolks.setContent(new DummyTabContent(getBaseContext()));
+        tHost.addTab(tSpecFolks);
+ 
+        /** Defining tab builder for Chats tab */
+        TabHost.TabSpec tSpecChats = tHost.newTabSpec("chats");
+        tSpecChats.setIndicator("Chats",getResources().getDrawable(R.drawable.chats));
+        tSpecChats.setContent(new DummyTabContent(getBaseContext()));
+        tHost.addTab(tSpecChats);
+ 
+        /** Defining tab builder for History tab */
+        TabHost.TabSpec tSpecHistory = tHost.newTabSpec("history");
+        tSpecHistory.setIndicator("History",getResources().getDrawable(R.drawable.history));
+        tSpecHistory.setContent(new DummyTabContent(getBaseContext()));
+        tHost.addTab(tSpecHistory);
+        
+        /** Defining tab builder for Profile tab */
+        TabHost.TabSpec tSpecProfile = tHost.newTabSpec("profile");
+        tSpecProfile.setIndicator("Profile",getResources().getDrawable(R.drawable.profile));
+        tSpecProfile.setContent(new DummyTabContent(getBaseContext()));
+        tHost.addTab(tSpecProfile);
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    
     
 }
